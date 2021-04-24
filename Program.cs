2623a14d61d5fcaf46ebb6fa3960c49af3bdf24a
Natnicha24 <natnicha.ant@mail.kmutt.ac.th>
2021-04-24 14:26:27 +0700

    num6 mid

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ws_mid3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char word;
+            word = char.Parse(Console.ReadLine());
+            int width, heigth, floor;
+            width = int.Parse(Console.ReadLine());
+            heigth = int.Parse(Console.ReadLine());
+            floor = int.Parse(Console.ReadLine());
+            int i, j, k,l,m,n;
+          /*  for (j = 0; j < heigth; j++) { 
+            for (i = 0; i < width; i++)
+            {
+                Console.Write(word);
+            }
+            Console.WriteLine();
+            }*/
+            for (k = 1; k <= floor; k++)
+            {
+                for (j = 0; j < heigth; j++)
+                {
+                    for (i = 0; i < width*k; i++)
+                    {
+                        Console.Write(word);
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+      
+    }
+}
